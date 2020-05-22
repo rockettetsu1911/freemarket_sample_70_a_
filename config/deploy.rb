ebdcc5d6d96692349457c1abb5ae83da49f3f8c1
rockettetsu1911 <rockettetsu@gmail.com>
2020-05-22 15:31:12 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@
 lock '3.14.0'
 
 # Capistranoのログの表示に利用する
-set :application, 'freemarket_sample_70_a'
+set :application, 'freemarket_sample_70_a_'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:rockettetsu1911/freemarket_sample_70_a_.git'
