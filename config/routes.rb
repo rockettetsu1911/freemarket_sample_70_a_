e52d17bd76c3f4adde6d78960b603237c7f42ebc
rockettetsu1911 <rockettetsu@gmail.com>
2020-05-29 16:03:54 +0900

    モデル、SCSS,yml,routesの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
-  resources :items, only: [:index, new]
+  resources :items, only: [:index, :new]
   resources :users, only: :show
+  resources :cards, only: [:new, :show]
 end
