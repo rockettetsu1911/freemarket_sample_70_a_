575a31b4287fd7716f5e6e1cc54649c0181ebfd9
rockettetsu1911 <rockettetsu@gmail.com>
2020-05-30 00:27:41 +0900

    add_basic_certification
    Basic認証の導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth, if: :production?
+  before_action :basic_auth
+
+  private
+
+  def production?
+    Rails.env.production?
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'freemarket_sample70a' && password == '0601'
+    end
+  end
 end
