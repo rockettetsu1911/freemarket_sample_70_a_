1c7ff6307fb4894005598649c66fa4f3327abccb
rockettetsu1911 <rockettetsu@gmail.com>
2020-06-06 02:44:44 +0900

    商品詳細ページの表示(ルーティング、ルート)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,5 @@
 class UsersController < ApplicationController
 
-  def show
-    
-    
+  def showw
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   } 
 
   root 'items#index'
-  resources :items, only: [:index, :new]
+  resources :items, only: [:index, :new, :show]
   resources :users, only: :show
   resources :cards, only: [:new, :show]
 
