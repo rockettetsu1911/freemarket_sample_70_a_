1ee6bcc6cfb1956e4add1ebd22ffca85b2552ea5
atyoshimatsu <at.yoshimatsu@gmail.com>
2020-06-13 18:05:50 +0900

    @userインスタンス変数の利用をしないように変更
    各コントローラでインスタンス変数の定義し忘れたままviewファイルで@userを使うことが多いため

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,28 +1,30 @@
 class AddressesController < ApplicationController
   def new
+    @user = User.find(current_user.id)
+    @address = Address.new
   end
 
   def create
-    @address = Address.new(address_params)
-    if @address.save
+    address = Address.create(address_params)
+    if address.save
       redirect_to user_path(current_user.id), notice: '発送元・送付先住所を登録しました'
     else
-      flash.now[:alert] = @address.errors.full_messages
+      flash.now[:alert] = address.errors.full_messages
       render :new and return
     end
   end
   
   def edit
     @user = User.find(current_user.id)
-    @address = Address.find_by(user_id: current_user.id)
+    @address = Address.find(params[:id])
   end
 
   def update
-    @address = Address.update(address_params)
-    if @address
+    address = Address.update(address_params)
+    if address
       redirect_to user_path(current_user.id), notice: '発送元・送付先住所を変更しました'
     else
-      flash.now[:alert] = @address.errors.full_messages
+      flash.now[:alert] = address.errors.full_messages
       render :edit and return
     end
   end
