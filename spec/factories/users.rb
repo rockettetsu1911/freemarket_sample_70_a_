a154208794b9f52fb1b1f09a2c03e38ad47d38ce
rockettetsu1911 <rockettetsu@gmail.com>
2020-06-14 18:03:35 +0900

    カード単体テスト

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :user do
-    
+    id                    {"1"}
+    nickname              {"テストユーザ"}
+    email                 {"testuser@gmail.com"}
+    first_name            {"太郎"} 
+    last_name             {"手洲戸"}
+    first_name_kana       {"タロウ"}
+    last_name_kana        {"テスト"}
+    birthday              {"1990/05/01"}
+    telephone             {"09012345678"}
+    introduction          {"サンプル自己紹介です"}
+    password              {"testuser"}
+    password_confirmation {"testuser"}
+
   end
 end
