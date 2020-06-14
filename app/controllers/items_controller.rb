769b6295d95448093edb1566680cd2b9cc38968d
rockettetsu1911 <rockettetsu@gmail.com>
2020-06-14 18:45:55 +0900

    クレジットカード機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,90 @@
 class ItemsController < ApplicationController
+  before_action :item_look_for, only: :purchase
+  require 'payjp'
 
   def index
+    @items = Item.all.last(3)
+    @likes_count = Like.group(:item_id).count
+
+    ctgry_ids = Category.roots.pluck(:id)
+    items_by_category = {}
+    ctgry_ids.each do |ctgry_id|
+      category_items_id = Category.find(ctgry_id).descendant_ids
+      items_by_category[:"#{Category.find(ctgry_id).name}"] = Item.where(category_id: category_items_id).last(3)
+    end
+
+    @picked_up_category = "レディース"
+    @picked_up_items = items_by_category[:"#{@picked_up_category}"]
+
   end
 
   def new
     @item = Item.new
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def purchase
+    @item = Item.find(params[:id])
+    @card = Card.find_by(user_id: current_user.id)
+    if @card == nil
+      redirect_to new_card_path
+      flash[:noCard] = "Cardが登録されていませんので登録してください"
+    else
+      Payjp.api_key = Rails.application.credentials[:payjp][:PAYJP_PRIVATE_KEY]
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      @card_information = customer.cards.retrieve(@card.card_id)
+
+      # 《＋α》 登録しているカード会社のブランドアイコンを表示するためのコードです。---------
+      @card_brand = @card_information.brand      
+      case @card_brand
+      when "Visa"
+        @card_src = "if-visa-2593666_86609.svg"
+      when "JCB"
+        @card_src = "jcbcard.png"
+      when "MasterCard"
+        @card_src = "mastercard.png"
+      when "American Express"
+        @card_src = "americancard.png"
+      when "Diners Club"
+        @card_src = "diners_club.png"
+      when "Discover"
+        @card_src = "discover.png"
+      end
+      # ---------------------------------------------------------------
+    end
+  end
+
+  def pay
+    @item = Item.find(params[:id])
+    card = Card.find_by(user_id: current_user.id)
+    Payjp.api_key = Rails.application.credentials[:payjp][:PAYJP_PRIVATE_KEY]
+    Payjp::Charge.create(
+      amount: @item.price, # Payjpに載る金額
+      customer: card.customer_id, # 顧客ID
+      currency: 'jpy'
+    )
+    @item.update(buyer: current_user.id)
+    require 'date'
+    @item.update(bought_at: Time.now )
+    redirect_to done_item_path(@item.id)
+    flash[:notice] = "購入が完了しました"
+  end
+
+  def done
+  end
+
+  private
+
+  def item_look_for
+    @item = Item.find(params[:id])
+    unless @item.bought_at == nil
+      redirect_to item_path(@item.id)
+      flash[:notice] = "この商品は売り切れです。"
+    end
+  end
+
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,30 @@ Rails.application.routes.draw do
   } 
 
   root 'items#index'
-  resources :items, only: [:index, :new]
+  resources :items, only: [:index, :new, :show] do
+    member do
+      get  'purchase'=>  'items#purchase', as: 'purchase'
+      patch 'pay'=>   'items#pay', as: 'pay'
+      get 'done'=>  'items#done', as: 'done'
+    end
+  end
   resources :users, only: :show
-  resources :cards, only: [:new, :show]
-
+  resources :cards, only: [:new, :create, :show, :destroy] 
+  resources :users do
+    resources :addresses, only: [:new, :create]
+  end
   resources :purchases, except: [:index, :new, :create, :show, :edit, :update, :destroy] do
     member do
       get 'confirm'
       #Todo: 購入完了ページなどが以下に追加される
     end
   end
+  
+
+  # resources :purchases, except: [:index, :new, :create, :show, :edit, :update, :destroy] do
+  #     member do
+  #       get 'confirm'
+  #       #Todo: 購入完了ページなどが以下に追加される
+  #   end
+  # end
 end
