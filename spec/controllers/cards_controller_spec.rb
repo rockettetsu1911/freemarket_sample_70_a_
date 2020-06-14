46145d9b610b572361fe67711eacf43f9318dfcf
rockettetsu1911 <rockettetsu@gmail.com>
2020-06-14 18:03:54 +0900

    カードコントローラーテスト

---
+++ spec/controllers/cards_controller_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe CardsController do
+  before do
+    @user = FactoryBot.create(:user)
+    describe 'GET #new' do
+      it "new.html.hamlに遷移すること" do
+        get :new
+        expect(response).to render_template :new
+      end
+    end
+    describe 'GET #show' do
+      it "show.html.hamlに遷移すること" do
+        @user = FactoryBot.create(:user)
+        get :show, params: {id: 1 }
+        expect(response).to render_template :show
+      end
+    end
+  end
+
+end
