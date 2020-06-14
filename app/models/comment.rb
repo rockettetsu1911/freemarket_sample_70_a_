eb8477249dadf27e51894758541a746949f37aae
rockettetsu1911 <rockettetsu@gmail.com>
2020-06-14 16:06:24 +0900

    fix_model_validates

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,5 +4,6 @@ class Comment < ApplicationRecord
   belongs_to :item
 
   validates :text, presence: true
+  validates :user_id, :item_id, presence:true
   validates :text, presence: { message: "コメントを入力してください" }
 end
