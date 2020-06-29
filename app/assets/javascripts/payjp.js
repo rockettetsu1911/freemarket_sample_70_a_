ba6e6fefd47dc24817a4dd1c8391029bdb5d3b56
rockettetsu1911 <rockettetsu@gmail.com>
2020-06-29 17:45:12 +0900

    consoleのエラー表示の解消

--- app/assets/javascripts/payjp.js
+++ app/assets/javascripts/payjp.js
@@ -1,33 +1,28 @@
 $(document).on('turbolinks:load', function() {
+  $('#token_submit').on('click', function(e) {
     Payjp.setPublicKey("pk_test_0df3d3eb150a3f3dd0f2fc3e");
-    const btn = document.getElementById('token_submit'); //IDがtoken_submitの場合に取得する
-    btn.addEventListener("click", (e) => {　//ボタンが押されたときに作動させる
-        e.preventDefault();　//ボタンを一旦無効化する
-
-      //カード情報生成
-      const card = {
-        number: document.getElementById("card_number").value,
-        cvc: document.getElementById("cvc").value,
-        exp_month: document.getElementById("exp_month").value,
-        exp_year: document.getElementById("exp_year").value
-      }; //入力されたデータを取得する
-
-      //cardトークン生成
-      Payjp.createToken(card, (status, response) => {
-        if (status === 200) { 
-          $("#card_number").removeAttr("name");
-          $("#cvc").removeAttr("name");
-          $("#exp_month").removeAttr("name");
-          $("#exp_year").removeAttr("name"); //カード情報を自分のサーバにpostせず削除する
-          $("#card_token").append(
-            $('<input hidden name="payjp-token">').val(response.id)
-          ); //トークンを送信できるように隠しタグを生成
-          document.inputForm.submit();
-          alert("登録が完了しました"); 
-        } else {
-          alert("カード情報が正しくありません。");
-        }
-      });
+    e.preventDefault();
+    //カード情報生成
+    const card = {
+      number: document.getElementById("card_number").value,
+      cvc: document.getElementById("cvc").value,
+      exp_month: document.getElementById("exp_month").value,
+      exp_year: document.getElementById("exp_year").value
+    }; //入力されたデータを取得します。
+    //トークン生成
+    Payjp.createToken(card, (status, response) => {
+      if (status === 200) { //成功した場合
+        $("#card_number").removeAttr("name");
+        $("#cvc").removeAttr("name");
+        $("#exp_month").removeAttr("name");
+        $("#exp_year").removeAttr("name"); //カード情報を自分のサーバにpostせず削除します
+        $("#card_token").append(
+          $('<input hidden name="payjp-token">').val(response.id)
+        ); //トークンを送信できるように隠しタグを生成
+        document.inputForm.submit();
+      }
     });
+  });
   false;
 });
+
