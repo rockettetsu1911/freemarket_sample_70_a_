4527b15be65eeb0ddeb7f6f74c3dea5a164c3027
rockettetsu1911 <rockettetsu@gmail.com>
2020-07-04 12:47:00 +0900

    コメントをdeleteアクションからpatchアクションへ切り替え

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,22 @@
 class CommentsController < ApplicationController
 
+  before_action :move_to_login, only: [:create]
+
   def create
     @comment = Comment.new(comment_params)
+    @current_user = current_user
+    @item = Item.find(@comment.item_id)
     if @comment.save
       respond_to do |format|
-        format.json 
+        format.json
       end
      else
        flash[:alert] = 'コメント出来ませんでした。'
-       redirect_to item_path(comment.item_id)
+       redirect_to item_path(@comment.item_id)
       end
   end
 
-  def destroy
+  def update
     comment = Comment.find(params[:id])
     comment.text = 'このコメントは出品者によって削除されました。'
     comment.comment_status = 1
@@ -24,5 +28,8 @@ private
     params.require(:comment).permit(:text,:item_id).merge(user_id: current_user.id)
   end
 
-  ## ユーザーがログインしていない場合のコントローラー？
+  def move_to_login
+    redirect_to user_session_path unless user_signed_in?
+  end
+  
 end
