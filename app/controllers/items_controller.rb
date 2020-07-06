9379d8b6380382c385237d9064ea688be5418249
rockettetsu1911 <rockettetsu@gmail.com>
2020-07-07 01:56:57 +0900

    カテゴリーの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,6 +9,7 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all.last(3)
     @likes_count = Like.group(:item_id).count
+    @parents = Category.where(ancestry: nil)
 
     ctgry_ids = Category.roots.pluck(:id)
     items_by_category = {}
