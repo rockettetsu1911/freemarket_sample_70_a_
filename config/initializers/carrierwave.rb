b058581834a34b7beb360713a1ab5baccfebcd06
rockettetsu1911 <rockettetsu@gmail.com>
2020-07-10 16:42:19 +0900

    fix_carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,9 +3,7 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  if Rails.env.development? || Rails.env.test?
-    config.storage = :file
-  elsif Rails.env.production
+  if Rails.env.development? 
     config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
@@ -14,8 +12,10 @@ CarrierWave.configure do |config|
       aws_secret_access_key: Rails.application.credentials[:aws][:secret_access_key],
       region: 'ap-northeast-1'
     }
+    config.fog_directory  = 'freemarketsample70a'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarketsample70a'
+  else 
+    config.storage :file # 開発環境:public/uploades下に保存
+    config.enable_processing = false if Rails.env.test? #test:処理をスキップ
   end
-
-  config.fog_directory  = 'freemarketsample70a'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarketsample70a'
 end
