17fd8939f3e09762372b15e070a1d9ddd5819364
atyoshimatsu <at.yoshimatsu@gmail.com>
2020-07-15 21:37:04 +0900

    商品がない場合の表示

--- app/assets/javascripts/items-lists.js
+++ app/assets/javascripts/items-lists.js
@@ -1,5 +1,4 @@
 $(document).on('turbolinks:load', function() {
-  console.log(location.pathname);
   if (location.pathname.match(/\/users\/[0-9]+\/sell_list/) != null) {
     $('.buy-list__link').css('background-color', '#eeeeee');
   } else if (location.pathname.match(/\/users\/[0-9]+\/buy_list/) != null) {
