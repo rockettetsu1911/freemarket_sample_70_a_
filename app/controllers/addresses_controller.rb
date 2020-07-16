9c40db154534ffee18fd2551449df4abbf1e89ec
rockettetsu1911 <rockettetsu@gmail.com>
2020-07-16 19:47:14 +0900

    住所変更ページに部分テンプレートの適用
    -スペルミスの修正

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,7 +1,10 @@
 class AddressesController < ApplicationController
+  before_action :set_parents, only: [:edit]
+
   def edit
     @user = User.find(current_user.id)
     @address = Address.find(params[:id])
+    @user_items_count = Item.where(user_id: current_user.id).count
   end
 
   def update
@@ -17,6 +20,10 @@ class AddressesController < ApplicationController
   private
   def address_params
     params.require(:address).permit(:dest_first_name, :dest_last_name, :dest_first_name_kana, :dest_last_name_kana, :zip_code, :prefecture, :city, :block_number, :building, :telephone, :user_id)
+  end
+
+  def set_parents
+    @parents = Category.where(ancestry: nil)
   end
 
 end
